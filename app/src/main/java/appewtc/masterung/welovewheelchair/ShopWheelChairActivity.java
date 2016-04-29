e0f0526fbd3facb1628c15dbbcd75a75756d2b77
masterUNG <phrombutr@gmail.com>
2016-04-29 15:40:39 +0700

    Get JSON finish

--- app/src/main/java/appewtc/masterung/welovewheelchair/ShopWheelChairActivity.java
+++ app/src/main/java/appewtc/masterung/welovewheelchair/ShopWheelChairActivity.java
@@ -1,7 +1,14 @@
 package appewtc.masterung.welovewheelchair;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class ShopWheelChairActivity extends AppCompatActivity {
 
@@ -9,5 +16,45 @@ public class ShopWheelChairActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop_wheel_chair);
+
+        ConnectedJSON connectedJSON = new ConnectedJSON();
+        connectedJSON.execute();
+
+    }   // Main Method
+
+    //Create Inner Class
+    public class ConnectedJSON extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url("http://swiftcodingthai.com/nuk/php_get_shop.php").build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+
+        }   // doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("29April", "JSON ==> " + s);
+
+        }   // onPost
+
+    }   // ConnectedJSON Class
+
+    public void clickBackShopWheelChair(View view) {
+        finish();
     }
-}
+
+}   // Main Class
